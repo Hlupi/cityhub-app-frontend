f472ef994c282ac0c03328d7a16eeb8cd1c8944a
ARYUNAP <AMI619847@GMAIL.com>
2018-07-26 12:23:06 +0200

    some if statements, still need to checl them

---
+++ src/components/events/Event.js
@@ -0,0 +1,43 @@
+import React, {PureComponent} from 'react'
+import { createDecipheriv } from 'crypto';
+
+
+export default class Event extends PureComponent {
+    state = {}
+
+    render() {
+            if (event.image===null && event.address===null) return (
+                <div>
+                    <p>{event.description}</p>
+                </div>
+            )
+            else if (event.address === null) return (
+                <div>
+                    <img src={event.image} alt="" />
+                    <p>{event.description}</p>
+                </div>
+            )
+            
+            else if (event.image === null) return (
+                <div>
+                    <p>{event.address}</p> 
+                    <p>{event.description}</p>
+                </div>
+            )            
+            else return (
+                <div>
+                    <img src={event.image} alt="" />
+                    <p>{event.address}</p> 
+                    <p>{event.description}</p>
+                </div>               
+            )
+    }       
+}
+
+// {
+//     if (event.image===null && event.address===null) return event.description
+//     else if (event.address === null) return event.image && event.description
+//     else if (event.image === null) return event.address && event.description
+//     else return (event.image&&event.address&&event.description) 
+// } 
+
